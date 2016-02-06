ce085321688dfb93c4d059166ba63e293114a915
Lucas Anderson <landerson@westernmilling.com>
2016-02-05 17:49:02 -0800

    Added userstamp to application controller
    Code Climate pointed out some warnings and methods
    that weren't being tested.
    
    * Updated the GEMFILE, which fixed vulnerabilities
      cycle_cout had.
    * Updated rubocop.yml to include the TargetRubyVersion
      and change TrailingComma to TrailingCommaInLiteral.
    * Removed duplicate method in application_controller.
    * Added application_policy_spec.rb
    * Added a scope test in user_policy_spec.rb

---
+++ spec/policies/application_policy_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe ApplicationPolicy, type: :policy do
+  let(:user) { build(:user) }
+  let(:record) { build(:user, :moderator) }
+
+  subject { ApplicationPolicy.new(user, record) }
+
+  describe 'index?' do
+    it { is_expected.not_to permit_action(:index) }
+  end
+
+  describe 'show?' do
+    it { is_expected.not_to permit_action(:show) }
+  end
+
+  describe 'new?' do
+    it { is_expected.not_to permit_action(:new) }
+  end
+
+  describe 'create?' do
+    it { is_expected.not_to permit_action(:create) }
+  end
+
+  describe 'edit?' do
+    it { is_expected.not_to permit_action(:edit) }
+  end
+
+  describe 'update?' do
+    it { is_expected.not_to permit_action(:update) }
+  end
+
+  describe 'destroy?' do
+    it { is_expected.not_to permit_action(:destroy) }
+  end
+
+  describe '.scope' do
+    subject { ApplicationPolicy::Scope.new(user, record).resolve }
+
+    it { is_expected.to eq record }
+  end
+end
