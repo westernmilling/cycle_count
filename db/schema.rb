997f4205c22da72c0d1eaa9523b37c190f44bce9
Lucas Anderson <landerson@westernmilling.com>
2016-03-03 12:46:27 -0800

    Switched database from mysql to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,72 +13,75 @@
 
 ActiveRecord::Schema.define(version: 20160226014344) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cycle_counts", force: :cascade do |t|
-    t.integer  "location_id",    limit: 4
-    t.datetime "requested_date",           null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "creator_id",     limit: 4
-    t.integer  "updater_id",     limit: 4
-    t.integer  "deleter_id",     limit: 4
+    t.integer  "location_id"
+    t.datetime "requested_date", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "creator_id"
+    t.integer  "updater_id"
+    t.integer  "deleter_id"
   end
 
   add_index "cycle_counts", ["location_id"], name: "index_cycle_counts_on_location_id", using: :btree
 
   create_table "locations", force: :cascade do |t|
-    t.integer  "location_number", limit: 4,   null: false
-    t.integer  "area_number",     limit: 4,   null: false
-    t.integer  "sequence_number", limit: 4,   null: false
-    t.string   "description",     limit: 255, null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "creator_id",      limit: 4
-    t.integer  "updater_id",      limit: 4
-    t.integer  "deleter_id",      limit: 4
+    t.integer  "location_number", null: false
+    t.integer  "area_number",     null: false
+    t.integer  "sequence_number", null: false
+    t.string   "description",     null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "creator_id"
+    t.integer  "updater_id"
+    t.integer  "deleter_id"
   end
 
   create_table "pallets", force: :cascade do |t|
-    t.integer  "cycle_count_id", limit: 4
-    t.string   "pallet_number",  limit: 255,                     null: false
-    t.string   "check_state",    limit: 255, default: "Pending"
-    t.string   "check_location", limit: 255
-    t.string   "check_result",   limit: 255
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
-    t.integer  "creator_id",     limit: 4
-    t.integer  "updater_id",     limit: 4
-    t.integer  "deleter_id",     limit: 4
+    t.integer  "cycle_count_id"
+    t.string   "pallet_number",                      null: false
+    t.string   "check_state",    default: "Pending"
+    t.string   "check_location"
+    t.string   "check_result"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.integer  "creator_id"
+    t.integer  "updater_id"
+    t.integer  "deleter_id"
   end
 
   add_index "pallets", ["cycle_count_id"], name: "index_pallets_on_cycle_count_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 255,             null: false
-    t.integer  "is_active",              limit: 4,   default: 1, null: false
-    t.string   "email",                  limit: 255,             null: false
-    t.string   "role_name",              limit: 255,             null: false
-    t.string   "encrypted_password",     limit: 255
-    t.string   "reset_password_token",   limit: 255
+    t.string   "name",                               null: false
+    t.integer  "is_active",              default: 1, null: false
+    t.string   "email",                              null: false
+    t.string   "role_name",                          null: false
+    t.string   "encrypted_password"
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0, null: false
+    t.integer  "sign_in_count",          default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "remember_created_at"
-    t.integer  "failed_attempts",        limit: 4,   default: 0, null: false
-    t.string   "unlock_token",           limit: 255
+    t.integer  "failed_attempts",        default: 0, null: false
+    t.string   "unlock_token"
     t.datetime "locked_at"
-    t.string   "invitation_token",       limit: 255
+    t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit",       limit: 4
-    t.integer  "invited_by_id",          limit: 4
-    t.string   "invited_by_type",        limit: 255
-    t.integer  "invitations_count",      limit: 4,   default: 0
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
